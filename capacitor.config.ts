22e640ed7278398598455e97cd3ca0f50a30751f
Marcel Bünger <marcel.buenger@gmx.de>
2025-07-22 10:52:08 +0200

    Version 0.9.9.6: PDF-Öffnen in nativer App verbessert

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'de.rwk.einbeck',
   appName: 'RWK Einbeck',
   webDir: '.next/static',
-  appVersion: '0.9.9.5',
+  appVersion: '0.9.9.6',
   server: {
     url: 'https://rwk-einbeck.de',
     cleartext: true
