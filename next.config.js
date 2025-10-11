f16a844dcf2c0ac04098b80eeb844bc7fdf8128b
Stephanie Bünger <marcel.buenger@gmx.de>
2025-10-11 15:27:43 +0200

    Fix undici webpack compatibility issue

--- next.config.js
+++ next.config.js
@@ -1,14 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    esmExternals: 'loose',
   },
 }
 
