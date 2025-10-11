ef490ae1d71f33ea7c961bb271e43c59efe07271
Stephanie Bünger <marcel.buenger@gmx.de>
2025-10-11 15:30:50 +0200

    Exclude undici from webpack bundling

--- next.config.js
+++ next.config.js
@@ -7,18 +7,22 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   webpack: (config, { isServer }) => {
+    config.externals = config.externals || [];
+    config.externals.push('undici');
+    
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
+        undici: false,
       };
     }
     return config;
   },
   experimental: {
-    esmExternals: 'loose',
+    serverComponentsExternalPackages: ['undici'],
   },
 }
 
